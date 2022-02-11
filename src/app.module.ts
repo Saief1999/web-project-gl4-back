191ec6e3364c37e03eecf18ef191d1dde8406a62
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-02-11 21:17:27 +0100

    fix conflict issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,11 @@ import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { CinemaModule } from './cinema/cinema.module';
 import { MailModule } from './mail/mail.module';
-<<<<<<< HEAD
 import { AccountsModule } from './accounts/accounts.module';
-=======
 import { MulterModule } from '@nestjs/platform-express';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path/posix';
 import { uploadDestination } from './utilities/upload';
->>>>>>> main
 
 @Module({
   imports: [
