ccf334f0a83bb9334d97a52517380a5ce5be612e
saief1999 <saief_zaneti@yahoo.com>
2022-02-11 11:37:25 +0100

    some modifications to dtos & utilities

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+export let app: INestApplication;
 
 async function bootstrap() {
   const corsOptions = {
     origin: [process.env.FRONTORIGIN],
     optionsSuccessStatus: 200,
   };
-  const app = await NestFactory.create(AppModule);
+  app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(3000);
